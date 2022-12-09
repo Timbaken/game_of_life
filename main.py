133002b2a7f8be587743a1401fcd4205a03b3861
Timbaken <94841602+Timbaken@users.noreply.github.com>
2022-12-09 19:42:57 +0100

    Vergeten deze mee te  committen

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from Visualisation import *
 from Simulator import *
 import time
 
+grid_width = 10
+grid_heigth = 10
+
 # Configuratie
 VISUALISATION=True
 # VISUALISATION=False
 
 if __name__ == "__main__":
-    w = World(10)
-    sim = Simulator(w)
+
+
+    w = World(grid_width, grid_heigth)
+    sim = Simulator(6, w, True)
 
     if VISUALISATION:
         vis = Visualisation(sim)
